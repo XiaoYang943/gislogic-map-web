d97659238131260663110ef0ed0ca0798af88423
XiaoYang943 <1352253543@qq.com>
2024-09-11 23:12:38 +0800

    fix:fix flatgeobuf

--- src/pages/examples/datasource/flatgeobuf/index.vue
+++ src/pages/examples/datasource/flatgeobuf/index.vue
@@ -1,4 +1,4 @@
-<script setup lang="ts">
+<script lang="ts" setup>
 import {onMounted, ref} from "vue";
 import VectorSource from "ol/source/Vector";
 import Map from "ol/Map";
@@ -6,18 +6,18 @@ import View from "ol/View";
 import {GeoJSON} from "ol/format";
 import {geojson} from "flatgeobuf";
 import {VectorImage} from "ol/layer";
-import TileLayer from "ol/layer/Tile.js";
-import {XYZ} from "ol/source";
 
 const mapRef = ref<HTMLElement>();
 onMounted(async () => {
   if (mapRef.value) {
+
     const source = new VectorSource({
       format: new GeoJSON(),
       loader: async function () {
-        const response = await fetch('https://flatgeobuf.org/test/data/UScounties.fgb')
-        // const response = await fetch('/public/test/data/UScounties.fgb')
+        // const response = await fetch('https://flatgeobuf.org/test/data/UScounties.fgb')
+        const response = await fetch('/public/test/data/UScounties.fgb')
         if (response.body) {
+          const start = new Date().getTime()
           for await (let json of geojson.deserialize(response.body)) {
             const feature = new GeoJSON().readFeature(json);
             if (feature) {
@@ -32,27 +32,18 @@ onMounted(async () => {
               console.error('Failed to create feature from GeoJSON:', json);
             }
           }
+          // call your function
+          const end = new Date().getTime()
+          console.log('cost is', `${end - start}ms`)
         } else {
           console.error('No response body received');
         }
       }
     })
+
     new Map({
       target: mapRef.value,
       layers: [
-        new TileLayer({
-          source: new XYZ({
-            url: 'http://t4.tianditu.com/DataServer?T=vec_w&tk=a9bbec302f2213e1fe33e9775187d021&x={x}&y={y}&l={z}',
-          }),
-          zIndex: 0,
-        }),
-
-        new TileLayer({
-          source: new XYZ({
-            url: 'http://t4.tianditu.com/DataServer?T=cva_w&tk=81b9a9cf3e4be1df181e7bf54c5949fc&x={x}&y={y}&l={z}',
-          }),
-          zIndex: 1,
-        }),
         new VectorImage({
           source: source,
         })
