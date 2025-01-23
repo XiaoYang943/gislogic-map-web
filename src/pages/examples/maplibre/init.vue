148cb507b00fd74b04d85e2939df27949ed85dac
heyiyang <heyiyang@linespace.cn>
2025-01-23 16:17:37 +0800

    feat: add maplibre-gl demo

---
+++ src/pages/examples/maplibre/init.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+  import {onMounted} from "vue";
+  import maplibregl from 'maplibre-gl';
+  import 'maplibre-gl/dist/maplibre-gl.css';
+  onMounted(() => {
+    var map = new maplibregl.Map({
+      container: 'map',
+      style: 'https://demotiles.maplibre.org/style.json',
+      center: [0,0],
+      zoom: 0
+    });
+  })
+</script>
+
+<template>
+  <div id='map' style='width: 100%; height: 100%;'></div>
+</template>
+
+<style scoped>
+
+</style>
