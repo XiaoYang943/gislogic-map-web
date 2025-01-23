007d8acd45a19464f01cae4221b3a7512c0d39d5
heyiyang <heyiyang@linespace.cn>
2025-01-23 16:43:52 +0800

    feat: add cesium demo

---
+++ src/pages/examples/cesium/init.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="cesiumContainer"></div>
+</template>
+
+<script setup>
+import * as Cesium from 'cesium';
+import { onMounted } from 'vue';
+onMounted(() => {
+  const viewer = new Cesium.Viewer('cesiumContainer');
+});
+</script>
+
+<style>
+html,body,#cesiumContainer{
+  width: 100%;
+  height: 100vh;
+  padding: 0;
+  margin: 0;
+  overflow: hidden;
+}
+</style>
